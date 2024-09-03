f0a65de899a1a1bea915f1b9fe14839cb6da612a
JaynAP1 <jerxonmjair@gmail.com>
2024-09-02 22:57:50 -0500

    :sparkles: Fix bug buttons russian rulete

--- Dia9/script/script.js
+++ Dia9/script/script.js
@@ -1,42 +1,41 @@
 //Pokedex
 
-
-
 function Completa(){
-    
-    document.getElementById("IdPoke").addEventListener("input", (e)=>{
-        let GetId = document.getElementById("IdPoke").value;
-    
-        let Api = "https://pokeapi.co/api/v2/pokemon/"
+    let IdActual = 1;
+    let Api = "https://pokeapi.co/api/v2/pokemon/"
 
-        fetch(Api+GetId)
+    function SearchPoke(id){  
+            
+        fetch(Api+id)
         .then(res => res.json())
         .then(Pokeapi=>{
 
-        let NombreId = document.getElementById("Nombre");
-        NombreId.innerHTML = `${Pokeapi.id} - <span>${Pokeapi.forms[0].name}</span>`;
+            let NombreId = document.getElementById("Nombre");
+            NombreId.innerHTML = `${Pokeapi.id} - <span>${Pokeapi.forms[0].name}</span>`;
 
-        let Gif = document.getElementById("Gif");
-        Gif.innerHTML = `
-        <audio autoplay src="${Pokeapi.cries.latest}">
-        </audio>
-        <img src="${Pokeapi.sprites.other.showdown.front_default}">`
+            let Gif = document.getElementById("Gif");
+            Gif.innerHTML = `
+            <audio autoplay src="${Pokeapi.cries.latest}">
+            </audio>
+            <img src="${Pokeapi.sprites.other.showdown.front_default}">`
 
-        let newID = 0
+            IdActual = Pokeapi.id;
 
-        document.getElementById("Before").addEventListener("click", (e) => {
-            newID = Pokeapi.id - 1
-            GetId = String(newID)
+            document.getElementById("Before").addEventListener("click", (e) => {
+                SearchPoke(IdActual-1)
+            })
+            document.getElementById("After").addEventListener("click",(e)=>{
+                SearchPoke(IdActual+1)
+            });
         })
-        document.getElementById("After").addEventListener("click",(e)=>{
-            newID = Pokeapi.id + 1
-            GetId = String(newID)
-        })
-        Completa()
-})
-})
+    }
+
+    document.getElementById("IdPoke").addEventListener("input", (e)=>{
+        let GetId = e.target.value;
+        SearchPoke(GetId)  
+    })
+    SearchPoke(IdActual)
 }
 Completa()
-
 
 
